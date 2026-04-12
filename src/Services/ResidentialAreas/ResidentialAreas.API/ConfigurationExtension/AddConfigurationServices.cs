16ffdee78430b652b1d21321751049dd7896ad03
Your Name <fahadbinaziznabil@gmail.com>
2026-04-12 22:53:17 +0600

    Deviding Service configuration and pipeline configuration.

---
+++ src/Services/ResidentialAreas/ResidentialAreas.API/ConfigurationExtension/AddConfigurationServices.cs
@@ -0,0 +1,25 @@
+﻿namespace ResidentialAreas.API.ConfigurationExtension
+{
+    public static class AddConfigurationServices
+    {
+        public static void AddCustomConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddCarter();
+
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+
+            });
+
+            builder.Services.AddDbContext<AreaDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "My API", Version = "v1" });
+            });
+        }
+    }
+}
--- src/Services/ResidentialAreas/ResidentialAreas.API/Program.cs
+++ src/Services/ResidentialAreas/ResidentialAreas.API/Program.cs
@@ -1,55 +1,15 @@
+using ResidentialAreas.API.ConfigurationExtension;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter();
 
-builder.Services.AddMediatR(cfg =>
-{
-    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
-
-});
-
-builder.Services.AddDbContext<AreaDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() { Title = "My API", Version = "v1" });
-});
+builder.AddCustomConfiguration();
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var dbContext = services.GetRequiredService<AreaDbContext>();
-        await dbContext.Database.MigrateAsync();
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while migrating the database.");
-    }
-}
-
-if(app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
-
-}
-
-
-
-// Configure the HTTP request pipeline.
-app.MapCarter();
+app.AddCustomPipeline().Wait();
 
 app.Run();
