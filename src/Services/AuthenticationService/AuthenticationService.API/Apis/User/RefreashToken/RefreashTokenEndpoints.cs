a661d42cb4244e54ac99070158edecaa3d1f85e6
Your Name <fahadbinaziznabil@gmail.com>
2026-05-04 18:13:22 +0600

    Refreash token functionality

---
+++ src/Services/AuthenticationService/AuthenticationService.API/Apis/User/RefreashToken/RefreashTokenEndpoints.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.API.Apis.User.RefreashToken
+{
+    public record RefreashTokenResponse(string AccessToken);
+
+    public class EmailValidator : AbstractValidator<string>
+    {
+        public EmailValidator()
+        {
+            RuleFor(email => email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+        }
+    }
+
+    public class RefreashTokenEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/user/refreash-token", async (HttpContext httpContext, [FromQuery] string email, ISender sender, IValidator<string> emailValidator) =>
+            {
+                var validationResult = emailValidator.Validate(email);
+                if (!validationResult.IsValid)
+                {
+                    return Results.ValidationProblem(validationResult.ToDictionary());
+                }
+
+
+                string? refreashToken = httpContext?.Request?.Cookies["refreshToken"]?.ToString() ?? null;
+                if (string.IsNullOrEmpty(refreashToken))
+                {
+                    return Results.Problem(statusCode: StatusCodes.Status400BadRequest, detail: "Refreash token is required");
+                }
+
+
+                var command = new RefreashTokenCommand(email, refreashToken);
+                var result = await sender.Send(command);
+
+                
+                if (result.Error != null)
+                {
+                    return Results.Problem(statusCode: StatusCodes.Status400BadRequest, detail: result.Error.Detail);
+                }
+
+
+                if(result.Result == null)
+                {
+                    return Results.Problem(statusCode: StatusCodes.Status500InternalServerError, detail: "An unexpected error occurred.");
+                }
+
+
+                httpContext?.Response.Headers.Append("Authorization", $"Bearer {result.Result.AccessToken}");
+                
+                return Results.Ok(result.Result!.AccessToken);
+            })
+                .WithName("RefreashToken")
+                .WithTags("User")
+                .AllowAnonymous();
+        }
+    }
+}
---
+++ src/Services/AuthenticationService/AuthenticationService.API/Apis/User/RefreashToken/RefreashTokenHandler.cs
@@ -0,0 +1,75 @@
+﻿using AuthenticationService.API.AuthenticationDbContest;
+using AuthenticationService.API.EntityModels;
+using AuthenticationService.API.Helpers.Authenticate;
+using AuthenticationService.API.Helpers.ErrorCarrier;
+using AuthenticationService.API.Helpers.RefreashTokenHelper;
+using CQRSPattern.CQRS;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationService.API.Apis.User.RefreashToken
+{
+    public record RefreashTokenCommand(string Email, string RefreashToken) : ICommand<RefreashTokenResult>;
+
+    public record RefreashTokenResult(RefreashTokenResponse? Result, ErrorCarrier? Error);
+
+    public class RefreashTokenHandler : ICommandHandler<RefreashTokenCommand, RefreashTokenResult>
+    {
+        private readonly AuthDbContext _authDbContext;
+        private readonly IAuthenticationTokenCreator _authenticationTokenCreator;
+
+        public RefreashTokenHandler(AuthDbContext authDbContext, IAuthenticationTokenCreator authenticationTokenCreator)
+        {
+            _authDbContext = authDbContext;
+            _authenticationTokenCreator = authenticationTokenCreator;
+        }
+
+        public async Task<RefreashTokenResult> Handle(RefreashTokenCommand request, CancellationToken cancellationToken)
+        {
+            // Validate the user exists
+            EntityModels.User? userExist = await _authDbContext.Users.AsNoTracking().Include(u => u.UserRoles).ThenInclude(ur => ur.Role).FirstOrDefaultAsync(u => u.Email == request.Email, cancellationToken);
+            if (userExist == null)
+            {
+                return new RefreashTokenResult(null, new ErrorCarrier()
+                {
+                    Title = "USER_NOT_FOUND",
+                    StatusCode = 404,
+                    Detail = $"No user found with email: {request.Email}"
+                });
+            }
+
+
+            // Validate the refresh token exists and is valid
+            EntityModels.RefreshToken? refreashToken = await _authDbContext.RefreshTokens.AsNoTracking()
+                .FirstOrDefaultAsync(rt => rt.UserId == userExist.Id && rt.RevokedAt==null && rt.ExpiresAt > DateTime.UtcNow, cancellationToken);
+            if (refreashToken == null)
+            {
+                return new RefreashTokenResult(null, new ErrorCarrier()
+                {
+                    Title = "REFRESH_TOKEN_NOT_FOUND",
+                    StatusCode = 403,
+                    Detail = $"No valid refresh token found for user with email: {request.Email}"
+                });
+            }
+
+            bool isVaidToken = await RefreashTokenGenerator.VerifyTokenAsync(request.RefreashToken, refreashToken.TokenHash);
+
+            if (!isVaidToken)
+            {
+                return new RefreashTokenResult(null, new ErrorCarrier()
+                {
+                    Title = "INVALID_REFRESH_TOKEN",
+                    StatusCode = 403,
+                    Detail = $"The provided refresh token is invalid for user with email: {request.Email}"
+                });
+            }
+
+
+            // Generate a new access token
+            UserPayload userPayload = new UserPayload(UserId: userExist.Id.ToString(), Username: userExist.Username, Email: userExist.Email, Roles: userExist.UserRoles.Select(ur => ur.Role!.Name).ToList());
+            string accessToken = await _authenticationTokenCreator.CreateToken(userPayload);
+
+
+            return new RefreashTokenResult(new RefreashTokenResponse(accessToken), null);
+        }
+    }
+}
