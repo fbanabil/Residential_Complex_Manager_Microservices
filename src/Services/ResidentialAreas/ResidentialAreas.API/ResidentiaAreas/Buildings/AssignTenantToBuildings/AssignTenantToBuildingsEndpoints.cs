d0a7c176182d707803eb55d41d7cadc0dc063f0d
Your Name <fahadbinaziznabil@gmail.com>
2026-05-12 20:46:04 +0600

    Assigning tenant to buildings

--- src/Services/ResidentialAreas/ResidentialAreas.API/ResidentiaAreas/Buildings/AssignTenantToBuildings/AssignTenantToBuildingsEndpoints.cs
+++ src/Services/ResidentialAreas/ResidentialAreas.API/ResidentiaAreas/Buildings/AssignTenantToBuildings/AssignTenantToBuildingsEndpoints.cs
@@ -1,6 +1,53 @@
-﻿namespace ResidentialAreas.API.ResidentiaAreas.Buildings.AssignTenantToBuildings
+﻿
+namespace ResidentialAreas.API.ResidentiaAreas.Buildings.AssignTenantToBuildings
 {
-    public class AssignTenantToBuildingsEndpoints
+    public record AssignTenantToBuildingRequest(long BuildingCode, string Email);
+    public record AssignTenantToBuildingResponse(bool Success, string Message);
+
+
+    public class AssignTenantToBuildingRequestValidatior : AbstractValidator<AssignTenantToBuildingRequest>
     {
+        public AssignTenantToBuildingRequestValidatior()
+        {
+            RuleFor(x => x.BuildingCode).GreaterThanOrEqualTo(2000000000).WithMessage("Building code must be greater than or equal to 2000000000.");
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("A valid email address is required.");
+            RuleFor(x => x.BuildingCode).LessThan(3000000000).WithMessage("Building code must be less than 3000000000.");
+        }
+    }
+
+    public class AssignTenantToBuildingsEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/building/assign-tenant", HandleAssignTenantToBuilding)
+                .WithName("AssignTenantToBuilding")
+                .WithTags("Buildings")
+                .WithSummary("Assigns a tenant to a building.")
+                .ProducesProblem(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization("AdminOrComplexManager");
+        }
+
+        private static async Task<IResult> HandleAssignTenantToBuilding(AssignTenantToBuildingRequest request, ISender sender, IValidator<AssignTenantToBuildingRequest> validator)
+        {
+            var validationResult = await validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                return Results.ValidationProblem(validationResult.ToDictionary());
+            }
+
+
+            var command = request.Adapt<AssignTenantToBuildingCommand>();
+            
+            
+            var result = await sender.Send(command);
+            if (result.Error is not null)
+            {
+                return Results.Problem(detail: result.Error.Detail, statusCode: result.Error.StatusCode, title: result.Error.Title);
+            }
+            var response = result.Result.Adapt<AssignTenantToBuildingResponse>();
+            
+            
+            return Results.Ok(response);
+        }
     }
 }
--- src/Services/ResidentialAreas/ResidentialAreas.API/ResidentiaAreas/Buildings/AssignTenantToBuildings/AssignTenantToBuildingsHandler.cs
+++ src/Services/ResidentialAreas/ResidentialAreas.API/ResidentiaAreas/Buildings/AssignTenantToBuildings/AssignTenantToBuildingsHandler.cs
@@ -1,6 +1,90 @@
-﻿namespace ResidentialAreas.API.ResidentiaAreas.Buildings.AssignTenantToBuildings
+﻿using ResidentialAreas.API.Grpc;
+using ResidentialAreas.API.Helpers.ErrorCarrier;
+
+namespace ResidentialAreas.API.ResidentiaAreas.Buildings.AssignTenantToBuildings
 {
-    public class AssignTenantToBuildingsHandler
+    public record AssignTenantToBuildingCommand(long BuildingCode, string Email) : ICommand<AssignTenantToBuildingResult>;
+
+    public record AssignTenantToBuildingResult(AssignTenantToBuildingResponse? Result, ErrorCarrier? Error);
+
+
+    public class AssignTenantToBuildingsHandler : ICommandHandler<AssignTenantToBuildingCommand, AssignTenantToBuildingResult>
     {
+        private readonly UserValidations.UserValidationsClient _userValidationsClient;
+        private readonly AreaDbContext _areaDbContest;
+
+
+        public AssignTenantToBuildingsHandler(UserValidations.UserValidationsClient userValidationsClient, AreaDbContext areaDbContest)
+        {
+            _userValidationsClient = userValidationsClient;
+            _areaDbContest = areaDbContest;
+        }
+
+
+        public async Task<AssignTenantToBuildingResult> Handle(AssignTenantToBuildingCommand request, CancellationToken cancellationToken)
+        {
+            // Validate the user by calling the User Validations gRPC service
+            GetUserResponse user = await _userValidationsClient.GetUserAsync(new GetUserRequest { Email = request.Email }, cancellationToken: cancellationToken);
+
+            // Check if the gRPC call was successful and if the user is valid
+            int statusCode = int.TryParse(user.Error?.StatusCode, out int code) ? code : 500;
+            if (statusCode != 200)
+            {
+                return new AssignTenantToBuildingResult(null, new ErrorCarrier
+                {
+                    Title = user.Error?.Title,
+                    StatusCode = statusCode,
+                    Detail = user.Error?.ErrorMessage
+                });
+            }
+
+
+
+            // Check if the user is verified
+            if (user.UserByRpc.IsUserVerified == false)
+            {
+                return new AssignTenantToBuildingResult(null, new ErrorCarrier
+                {
+                    Title = "USER_NOT_VERIFIED",
+                    StatusCode = 403,
+                    Detail = "The user must be verified to be assigned to a building"
+                });
+            }
+
+
+            // Retrieve the building from the database using the provided building code
+            Building? building = await _areaDbContest.Buildings.AsNoTracking().FirstOrDefaultAsync(b => b.Code == request.BuildingCode, cancellationToken);
+            if (building == null)
+            {
+                return new AssignTenantToBuildingResult(null, new ErrorCarrier
+                {
+                    Title = "BUILDING_NOT_FOUND",
+                    StatusCode = 404,
+                    Detail = $"No building found with code {request.BuildingCode}"
+                });
+            }
+
+
+
+            // Update the building's tenant information in the database
+            try
+            {
+                await _areaDbContest.Buildings.Where(b => b.Id == building.Id)
+                    .ExecuteUpdateAsync(b => b.SetProperty(p => p.TenantId, Guid.Parse(user.UserByRpc.Id)), cancellationToken);
+            }
+            catch
+            {
+                return new AssignTenantToBuildingResult(null, new ErrorCarrier
+                {
+                    Title = "DATABASE_ERROR",
+                    StatusCode = 500,
+                    Detail = "An error occurred while updating the building's tenant information"
+                });
+            }
+
+
+            return new AssignTenantToBuildingResult(new AssignTenantToBuildingResponse(Success : true, Message : "Tenant successfully assigned to the building"), null);
+
+        }
     }
 }
