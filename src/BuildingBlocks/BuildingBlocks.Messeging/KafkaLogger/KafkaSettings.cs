75090ddf4a324daed075f160330b401f3fe472ba
Your Name <fahadbinaziznabil@gmail.com>
2026-05-21 21:25:51 +0600

    Getting started with kafka logger

---
+++ src/BuildingBlocks/BuildingBlocks.Messeging/KafkaLogger/KafkaSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace BuildingBlocks.Messaging.KafkaLogger
+{
+    public class KafkaSettings
+    {
+        public string BootstrapServers { get; set; } = "9092";
+        public string Topic { get; set; } = "app-logs";
+        public string Environment { get; set; } = "Docker";
+        public string? ServiceName { get; set; }
+        public string MinLevel { get; set; } = "Information";
+        public int QueueCapacity { get; set; } = 1000;
+
+
+        public KafkaSettings(string serviceName)
+        {
+            ServiceName = serviceName;
+        }
+
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Messeging/KafkaLogger/LogModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Messaging.KafkaLogger
+{
+    public class LogModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? ServiceName { get; set; }
+        public string? Environment { get; set; }
+        public string? LogLevel { get; set; }
+        public string? Message { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public string? Exception { get; set; }
+        public Guid? UserID {  get; set; }
+        public string? UserRole { get; set; }
+        public string? UserName { get; set; }
+        public Dictionary<string, string?> Details { get; set; } = new();
+    }
+}
